6cb5d7fc045aa495ea436ec6826bd30c2af464cd
vtlinh2510 <vuthuylinh2525@gmail.com>
2024-10-28 15:08:28 +0700

    Adds test  5 + 5

--- main-test.py
+++ main-test.py
@@ -3,7 +3,15 @@ from main import Add
 
 def TestAdd():
         assert Add(2,3) == 5
+        assert Add(5,5) == 11
+
         print("Add Function works correctly")
 
 if __name__ == '__main__':
         TestAdd()
+
+
+# git checkout -b add-test
+# git add .
+# git commit -m "Adds test  5 + 5 "
+# git push --set-upstream origin add-test        
